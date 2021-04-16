61a65ca6d5bf03b3468b73c401177744eac2ffec
Cristina <apostol.cristina1@gmail.com>
2021-04-16 16:43:15 +0300

    formular checkout (tari si orase)

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,10 +13,18 @@ export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
 
-  totalPrice: number =0;
+  totalPrice: number = 0;
   totalQuantity: number = 0;
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
 
-  constructor(private formBuilder: FormBuilder) { }
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  constructor(private formBuilder: FormBuilder,
+    private luv2ShopFormService: Luv2ShopFormService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -48,19 +59,91 @@ export class CheckoutComponent implements OnInit {
         expirationYear: [''],
       }),
     });
+
+    //populam lunile cardului
+    const startMonth: number = new Date().getMonth() + 1; //numaratoarea incepe de la 0
+    console.log("startMonth: " + startMonth);
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months" + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+    //populam anii cardului
+    this.luv2ShopFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card months" + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
+
+    //populam tarile
+
+    this.luv2ShopFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    );
   }
+
 
   onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
   }
 
-  copyShippingAddresstoBillingAddress(event){
-    if(event.target.checked){
+  copyShippingAddresstoBillingAddress(event) {
+    if (event.target.checked) {
       this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value);
     } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
     }
+  }
 
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    //daca anul curent este anul selectat, atunci incepe cu luna curenta
+
+    let startMonth: number;
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.luv2ShopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`{formGroupName} country code: ${countryCode}`);
+    console.log(`{formGroupName} country code: ${countryName}`);
+
+    this.luv2ShopFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if(formGroupName === 'shippingAddress') {
+            this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        //selecteaza primul element ca implicit
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
   }
 }
